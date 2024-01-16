4f1507bff9ae7a6e3c45a88bf07faab900470e37
RangaVittala SR <rangavittala.sr@gmail.com>
2024-01-16 18:27:31 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,43 @@
+'''
+x = 1
+y = 1
+z = x + y
+print(z)
+
+
+x = input("What's the value of x? ")
+y = input("What's the value of y? ")
+z = x + y
+print(z)
+
+z = int(x) + int(y)
+print(z)
+
+x = int(input("What's the value of x? "))
+y = int(input("What's the value of y? "))
+print(x + y)
+
+
+x = float(input("What's the value of x? "))
+y = float(input("What's the value of y? "))
+print(x + y)
+
+z = round(x + y)
+print(z)
+
+print(f"{z:,}")
+
+
+
+x = float(input("What's the value of x? "))
+y = float(input("What's the value of y? "))
+
+z = round(x + y)
+
+print(f"{z:,}")
+
+d = round(x + y, 3)
+print(d)
+print(f"{d:.3f}")
+
+'''
